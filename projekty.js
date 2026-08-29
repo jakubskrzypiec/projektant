11a64908fe1abff02e01248724ee9782ddb093cf
Jakub Skrzypiec <kuba.skrzypiec5@gmail.com>
2026-08-29 02:47:24 +0200

    Add files via upload

---
+++ projekty.js
@@ -0,0 +1,333 @@
+const projects = [
+  {
+    "title": "DOM Z ANTRESOLA",
+    "category": "PROJEKT WNĘTRZA",
+    "description": "Dwupoziomowa strefa dzienna łączy naturalne drewno, cegłę i czarne stalowe akcenty z jasną, spokojną bazą. Otwarta kuchnia i salon pod antresolą budują przestronne wnętrze, a widoczne belki oraz zieleń dodają mu ciepła i charakteru. Całość uzupełniają proste zabudowy i neutralne materiały w łazience oraz komunikacji.",
+    "folder": "01_Dom-z-antresola",
+    "cover": "01_Dom-z-antresola_00_GLOWNE.jpg",
+    "images": [
+      "01_Dom-z-antresola_00_GLOWNE.jpg",
+      "01_Dom-z-antresola_01.jpg",
+      "01_Dom-z-antresola_02.jpg",
+      "01_Dom-z-antresola_03.jpg",
+      "01_Dom-z-antresola_04.jpg",
+      "01_Dom-z-antresola_05.jpg",
+      "01_Dom-z-antresola_06.jpg",
+      "01_Dom-z-antresola_07.jpg",
+      "01_Dom-z-antresola_08.jpg",
+      "01_Dom-z-antresola_09.jpg"
+    ],
+    "alts": [
+      "salon i jadalnia pod antresolą",
+      "kuchnia z drewnianą zabudową",
+      "kuchnia z wyspą",
+      "strefa dzienna z antresolą",
+      "salon pod wysokim sufitem",
+      "schody na antresolę",
+      "łazienka z drewnianą zabudową",
+      "jasna łazienka z prysznicem",
+      "zabudowa gospodarcza",
+      "zabudowa w korytarzu"
+    ]
+  },
+  {
+    "title": "APARTAMENT LOFT",
+    "category": "PROJEKT WNĘTRZA",
+    "description": "Ciemny, loftowy apartament opiera się na graficie, czerni i surowych strukturach przełamanych głębokim bordo. Metalowe detale, kamienne powierzchnie i techniczne oświetlenie podkreślają industrialny charakter strefy dziennej. W prywatnych pomieszczeniach pojawiają się cieplejsze drewno i zieleń, dzięki czemu wnętrze pozostaje wyraziste, ale nie chłodne.",
+    "folder": "02_Apartament-loft-dark",
+    "cover": "02_Apartament-loft-dark_00_GLOWNE.jpg",
+    "images": [
+      "02_Apartament-loft-dark_00_GLOWNE.jpg",
+      "02_Apartament-loft-dark_01.jpg",
+      "02_Apartament-loft-dark_02.jpg",
+      "02_Apartament-loft-dark_03.jpg",
+      "02_Apartament-loft-dark_04.jpg",
+      "02_Apartament-loft-dark_05.jpg",
+      "02_Apartament-loft-dark_06.jpg",
+      "02_Apartament-loft-dark_07.jpg",
+      "02_Apartament-loft-dark_08.jpg",
+      "02_Apartament-loft-dark_09.jpg"
+    ],
+    "alts": [
+      "ciemna strefa dzienna z jadalnią",
+      "salon i jadalnia w loftowej stylistyce",
+      "jadalnia z bordową zabudową",
+      "widok na strefę dzienną z góry",
+      "gabinet z zieloną zabudową",
+      "sypialnia z miejscem do pracy",
+      "łazienka z kamiennym akcentem",
+      "ciemna garderoba",
+      "garderoba z lustrem",
+      "minimalistyczny przedpokój"
+    ]
+  },
+  {
+    "title": "PODDASZE Z BELKAMI",
+    "category": "PROJEKT WNĘTRZA",
+    "description": "Projekt wykorzystuje charakter poddasza i pozostawia drewniane belki jako najważniejszy element wnętrza. Jasne drewno, złamane biele i przygaszona zieleń są zestawione z cegłą oraz roślinnymi motywami, tworząc ciepłą i swobodną całość. Zaokrąglone zabudowy i miękkie formy mebli równoważą wyrazistą konstrukcję dachu.",
+    "folder": "03_Poddasze-z-belkami",
+    "cover": "03_Poddasze-z-belkami_00_GLOWNE.jpg",
+    "images": [
+      "03_Poddasze-z-belkami_00_GLOWNE.jpg",
+      "03_Poddasze-z-belkami_01.jpg",
+      "03_Poddasze-z-belkami_02.jpg",
+      "03_Poddasze-z-belkami_03.jpg",
+      "03_Poddasze-z-belkami_04.jpg",
+      "03_Poddasze-z-belkami_05.jpg",
+      "03_Poddasze-z-belkami_06.jpg",
+      "03_Poddasze-z-belkami_07.jpg",
+      "03_Poddasze-z-belkami_08.jpg"
+    ],
+    "alts": [
+      "salon pod drewnianymi belkami",
+      "salon z ceglaną ścianą",
+      "kuchnia i jadalnia na poddaszu",
+      "kuchnia z przeszkloną zabudową",
+      "sypialnia z roślinną tapetą",
+      "sypialnia z jasną zabudową",
+      "pokój pod skosem",
+      "przedpokój z zieloną wnęką",
+      "łazienka z zielonym akcentem"
+    ]
+  },
+  {
+    "title": "APARTAMENT Z WIDOKIEM",
+    "category": "PROJEKT WNĘTRZA",
+    "description": "Jasny apartament został zaprojektowany tak, aby pierwszoplanową rolę odgrywały duże przeszklenia i widok na miasto. Kremowe tkaniny, delikatne drewno i spokojne beże tworzą elegancką bazę, którą przełamują ciemniejsze, kamienne akcenty. Wnętrze jest lekkie i uporządkowane, z subtelną nutą hotelowego charakteru.",
+    "folder": "04_Apartament-z-widokiem",
+    "cover": "04_Apartament-z-widokiem_00_GLOWNE.jpg",
+    "images": [
+      "04_Apartament-z-widokiem_00_GLOWNE.jpg",
+      "04_Apartament-z-widokiem_01.jpg",
+      "04_Apartament-z-widokiem_02.jpg",
+      "04_Apartament-z-widokiem_03.jpg",
+      "04_Apartament-z-widokiem_04.jpg",
+      "04_Apartament-z-widokiem_05.jpg",
+      "04_Apartament-z-widokiem_06.jpg",
+      "04_Apartament-z-widokiem_07.jpg"
+    ],
+    "alts": [
+      "jadalnia przy panoramicznym oknie",
+      "salon i jadalnia w jasnych barwach",
+      "salon z widokiem na miasto",
+      "sypialnia z przeszkloną garderobą",
+      "zabudowa przy panoramicznym oknie",
+      "jasna łazienka",
+      "korytarz apartamentu",
+      "pralnia w zabudowie"
+    ]
+  },
+  {
+    "title": "MIESZKANIE BORDO",
+    "category": "PROJEKT WNĘTRZA",
+    "description": "Ciepłe drewno i spokojne beże zostały tutaj zestawione z bordowymi detalami oraz czarnymi, metalowymi podziałami. Otwarta strefa dzienna ma graficzny charakter, ale dzięki naturalnym materiałom i miękkim tkaninom pozostaje przytulna. Ten sam język kolorystyczny przechodzi do sypialni, komunikacji i łazienki, tworząc spójną całość.",
+    "folder": "05_Mieszkanie-bordo",
+    "cover": "05_Mieszkanie-bordo_00_GLOWNE.jpg",
+    "images": [
+      "05_Mieszkanie-bordo_00_GLOWNE.jpg",
+      "05_Mieszkanie-bordo_01.jpg",
+      "05_Mieszkanie-bordo_02.jpg",
+      "05_Mieszkanie-bordo_03.jpg",
+      "05_Mieszkanie-bordo_04.jpg",
+      "05_Mieszkanie-bordo_05.jpg",
+      "05_Mieszkanie-bordo_06.jpg",
+      "05_Mieszkanie-bordo_07.jpg",
+      "05_Mieszkanie-bordo_08.jpg",
+      "05_Mieszkanie-bordo_09.jpg"
+    ],
+    "alts": [
+      "salon i jadalnia z bordowymi akcentami",
+      "salon z metalowym przepierzeniem",
+      "jadalnia i kuchnia z drewnem",
+      "jadalnia przy oknie",
+      "kuchnia z bordową zabudową",
+      "sypialnia z bordowym zagłówkiem",
+      "drewniana zabudowa przy sypialni",
+      "przedpokój z zabudową",
+      "łazienka z bordowym grzejnikiem",
+      "łazienka z prysznicem"
+    ]
+  },
+  {
+    "title": "LOFT CEGLANY",
+    "category": "PROJEKT WNĘTRZA",
+    "description": "Loft łączy odsłoniętą konstrukcję dachu, cegłę i surowe struktury z miękkimi, naturalnymi kolorami. Drewniane belki i podłogi ocieplają przestrzeń, a proste meble oraz rzeźbiarskie oświetlenie nadają jej współczesny charakter. Poszczególne pomieszczenia różnią się nastrojem, ale zachowują wspólną, lekko industrialną bazę.",
+    "folder": "06_Loft-ceglany",
+    "cover": "06_Loft-ceglany_00_GLOWNE.jpg",
+    "images": [
+      "06_Loft-ceglany_00_GLOWNE.jpg",
+      "06_Loft-ceglany_01.jpg",
+      "06_Loft-ceglany_02.jpg",
+      "06_Loft-ceglany_03.jpg",
+      "06_Loft-ceglany_04.jpg",
+      "06_Loft-ceglany_05.jpg",
+      "06_Loft-ceglany_06.jpg",
+      "06_Loft-ceglany_07.jpg",
+      "06_Loft-ceglany_08.jpg"
+    ],
+    "alts": [
+      "salon pod odsłoniętymi belkami",
+      "kuchnia otwarta na salon",
+      "jadalnia przy kamiennej ścianie",
+      "jadalnia z ceglaną ścianą",
+      "salon z miękką sofą",
+      "sypialnia z ceglaną ścianą",
+      "pokój z miejscem do pracy",
+      "jasna kuchnia z wyspą",
+      "łazienka pod skosem"
+    ]
+  },
+  {
+    "title": "INDUSTRIAL GREEN",
+    "category": "PROJEKT WNĘTRZA",
+    "description": "Industrialna baza z betonu, grafitu i czarnych detali została ożywiona głęboką, oliwkową zielenią. Techniczne oświetlenie i metalowe podziały podkreślają zdecydowany charakter kuchni oraz salonu, a drewniana podłoga dodaje im cieplejszego tonu. W łazience ciemne powierzchnie i punktowe światło budują bardziej nastrojową, kompaktową przestrzeń.",
+    "folder": "07_Industrial-green",
+    "cover": "07_Industrial-green_00_GLOWNE.jpg",
+    "images": [
+      "07_Industrial-green_00_GLOWNE.jpg",
+      "07_Industrial-green_01.jpg",
+      "07_Industrial-green_02.jpg",
+      "07_Industrial-green_03.jpg",
+      "07_Industrial-green_04.jpg",
+      "07_Industrial-green_05.jpg",
+      "07_Industrial-green_06.jpg",
+      "07_Industrial-green_07.jpg"
+    ],
+    "alts": [
+      "salon z oliwkową sofą",
+      "salon z zieloną sofą",
+      "aneks kuchenny z zieloną zabudową",
+      "grafitowa kuchnia",
+      "kuchnia za czarnymi lamelami",
+      "ciemna łazienka z prysznicem",
+      "łazienka z grafitową zabudową",
+      "detal prysznica"
+    ]
+  },
+  {
+    "title": "APARTAMENT GLAMOUR",
+    "category": "PROJEKT WNĘTRZA",
+    "description": "Jasny apartament glamour bazuje na kremach, pudrowym różu i delikatnych złotych detalach. Duży roślinny motyw na ścianie staje się dekoracyjnym tłem dla miękkiej strefy wypoczynkowej, a zaokrąglone zabudowy łagodzą geometrię wnętrza. Całość jest lekka, elegancka i celowo pozbawiona ciężkich kontrastów.",
+    "folder": "08_Apartament-glamour",
+    "cover": "08_Apartament-glamour_00_GLOWNE.jpg",
+    "images": [
+      "08_Apartament-glamour_00_GLOWNE.jpg",
+      "08_Apartament-glamour_01.jpg",
+      "08_Apartament-glamour_02.jpg",
+      "08_Apartament-glamour_03.jpg",
+      "08_Apartament-glamour_04.jpg",
+      "08_Apartament-glamour_05.jpg",
+      "08_Apartament-glamour_06.jpg"
+    ],
+    "alts": [
+      "jasny salon z różowym fotelem",
+      "salon z roślinną tapetą",
+      "salon i kuchnia w jasnej kolorystyce",
+      "strefa dzienna od strony telewizora",
+      "zaokrąglona zabudowa",
+      "przejście przy jasnej kuchni",
+      "jasna zabudowa meblowa"
+    ]
+  },
+  {
+    "title": "MIESZKANIE Z JODELKA",
+    "category": "PROJEKT WNĘTRZA",
+    "description": "Wnętrze opiera się na ciepłej, eleganckiej bazie z drewnianą podłogą układaną w jodełkę. Beże i złamane biele zestawiono z czarnymi liniami oświetlenia oraz pojedynczymi, głębszymi akcentami kolorystycznymi. Proste zabudowy, marmurowe powierzchnie i oszczędne detale tworzą spokojną, dopracowaną przestrzeń.",
+    "folder": "09_Mieszkanie-jodelka",
+    "cover": "09_Mieszkanie-jodelka_00_GLOWNE.jpg",
+    "images": [
+      "09_Mieszkanie-jodelka_00_GLOWNE.jpg",
+      "09_Mieszkanie-jodelka_01.jpg",
+      "09_Mieszkanie-jodelka_02.jpg",
+      "09_Mieszkanie-jodelka_03.jpg",
+      "09_Mieszkanie-jodelka_04.jpg"
+    ],
+    "alts": [
+      "jadalnia na podłodze w jodełkę",
+      "strefa dzienna z bordową zasłoną",
+      "sypialnia w ciepłych beżach",
+      "łazienka z marmurową okładziną",
+      "korytarz z podłogą w jodełkę"
+    ]
+  },
+  {
+    "title": "MIESZKANIE BEZOWE",
+    "category": "PROJEKT WNĘTRZA",
+    "description": "Spokojne, beżowe mieszkanie zostało utrzymane w miękkiej, niemal monochromatycznej palecie. Jasne zabudowy, naturalne drewno i ciepłe światło porządkują niewielką przestrzeń, a czarne linie oświetlenia dodają jej nowoczesnego rytmu. Funkcjonalne rozwiązania w kuchni, gabinecie i łazience pozostają dyskretne i spójne z minimalistycznym charakterem całości.",
+    "folder": "10_Mieszkanie-bezowe",
+    "cover": "10_Mieszkanie-bezowe_00_GLOWNE.jpg",
+    "images": [
+      "10_Mieszkanie-bezowe_00_GLOWNE.jpg",
+      "10_Mieszkanie-bezowe_01.jpg",
+      "10_Mieszkanie-bezowe_02.jpg",
+      "10_Mieszkanie-bezowe_03.jpg",
+      "10_Mieszkanie-bezowe_04.jpg",
+      "10_Mieszkanie-bezowe_05.jpg",
+      "10_Mieszkanie-bezowe_06.jpg",
+      "10_Mieszkanie-bezowe_07.jpg"
+    ],
+    "alts": [
+      "salon z otwartą kuchnią",
+      "beżowy salon przy oknie",
+      "sypialnia w ciepłych beżach",
+      "kompaktowa jasna kuchnia",
+      "jadalnia w beżowej wnęce",
+      "gabinet z miejscem do pracy",
+      "jasny korytarz",
+      "łazienka z pralnią"
+    ]
+  }
+];
+
+const projectsGrid = document.querySelector("[data-projects-grid]");
+
+const projectImagePath = (project, fileName) => `./${fileName}`;
+
+const createProjectCard = (project, index) => {
+  const article = document.createElement("article");
+  article.className = "projects-page-card reveal";
+  article.dataset.projectCard = "";
+  article.dataset.category = project.category;
+  article.dataset.title = project.title;
+  article.dataset.description = project.description;
+  article.dataset.image = projectImagePath(project, project.cover);
+  article.dataset.gallery = project.images.map(file => projectImagePath(project, file)).join(",");
+  article.dataset.galleryAlts = project.alts.join("|");
+
+  const button = document.createElement("button");
+  button.className = "projects-page-card__button";
+  button.type = "button";
+  button.setAttribute("aria-label", `Otwórz projekt ${project.title}`);
+
+  const media = document.createElement("span");
+  media.className = "projects-page-card__media";
+
+  const image = document.createElement("img");
+  image.src = projectImagePath(project, project.cover);
+  image.alt = `${project.title} - ${project.alts[0] || "widok wnętrza"}`;
+  image.loading = "lazy";
+  image.decoding = "async";
+  media.append(image);
+
+  const copy = document.createElement("span");
+  copy.className = "projects-page-card__copy";
+
+  const eyebrow = document.createElement("span");
+  eyebrow.className = "projects-page-card__eyebrow";
+  eyebrow.textContent = project.category;
+
+  const title = document.createElement("span");
+  title.className = "projects-page-card__title";
+  title.textContent = project.title;
+
+  copy.append(eyebrow, title);
+  button.append(media, copy);
+  article.append(button);
+  return article;
+};
+
+if (projectsGrid) {
+  const fragment = document.createDocumentFragment();
+  projects.forEach((project, index) => fragment.append(createProjectCard(project, index)));
+  projectsGrid.append(fragment);
+}
